9f7b856f1e0c1faf9106d36082a3d6f29fe63006
TonyJiangWJ <tonyjiangwj@gmail.com>
2017-03-07 22:53:51 +0800

    稍作优化 不再那么麻烦

--- jxgl.js
+++ jxgl.js
@@ -1,8 +1,8 @@
 
 function importJquery(){
- var jquery = document.createElement('script');  
-jquery.src = "https://code.jquery.com/jquery-3.1.1.min.js";
-document.getElementsByTagName('head')[0].appendChild(jquery);    
+	var jquery = document.createElement('script');  
+	jquery.src = "https://code.jquery.com/jquery-3.1.1.min.js";
+	document.getElementsByTagName('head')[0].appendChild(jquery); 
 }
 
 function checkShiJian(){
@@ -54,15 +54,47 @@ function getTotalXueFen(){
 	}
 	console.log("总学分："+sum)
 }
-// 先粘贴上面的 回车
 
-importJquery()// 然后输入这个 插入jQuery
+function switchToNextTerm(){
+	var yearNow = jQuery('#ddlxn>option:checked').index()
+	var termNow = $('#ddlxq').val()
+	console.log("now:"+jQuery('#ddlxn>option:checked').text()+"第"+termNow+"学期")
+	if(termNow=='2'){
+		jQuery('#ddlxn').children()[yearNow+1].selected=true
+		$('#ddlxq').val(1)
+	}else{
+		$('#ddlxq').val(2)
+	}
+	$('#btnCx').click()
+}
 
-//最后粘贴下面4个，回车
-checkShiJian()
-checkRenXuan()
-checkBiXiu()
-getTotalXueFen()
+function doMain(){
+	checkShiJian();
+	checkRenXuan()
+	checkBiXiu()
+	getTotalXueFen()
+	console.log("done")
+	switchToNextTerm()
+}
 
+function installJquery(callback){
+	importJquery()
+	console.log("inside installJquery")
+	callback("安装jQuery")
+}
 
-//点选每个学期都要重新进行上述操作
+installJquery(function f(s){
+	console.log(s)
+})
+//先复制以上代码到console 回车执行
+
+//然后再执行doMain()
+doMain()
+
+/***
+  * 进入大一第一个学期
+  * 执行上述操作
+  * 当页面刷新后只要按两次键盘向上键，到方法定义代码再回车，然后再按两次向上键回车执行doMain()
+  * 到最后一个学期之后 将log保存到本地 在对log文件进行处理即刻 结合sublime和Excel处理一下就行了，easy
+  */
+
